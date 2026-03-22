8002a014856aa1edce0bad1c0a3e25a92933289a
talentlessmoll <mollyrobbin316@gmail.com>
2026-03-22 15:26:40 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,10 +2,13 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
     const targetUrl = url.searchParams.get("url");
-    if (!targetUrl) return new Response("Hexaro API Proxy", { status: 200 });
+    if (!targetUrl) return new Response("API Bridge Active", { status: 200 });
 
     const response = await fetch(targetUrl, {
-      headers: { "User-Agent": "Mozilla/5.0", "Referer": "https://streamed.pk/" }
+      headers: { 
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/124.0.0.0",
+        "Referer": "https://streamed.pk/" 
+      }
     });
 
     const nH = new Headers(response.headers);
